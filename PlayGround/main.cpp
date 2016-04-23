9929d9c17615b6d25d0fde1d374326070ff46818
Manoj Chakkaravarthy <manojchakkaravarthy@manojs-macbook-pro.local>
2016-04-23 13:06:50 -0700

    Initial Commit

---
+++ PlayGround/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PlayGround
+//
+//  Created by Manoj Chakkaravarthy on 4/23/16.
+//  Copyright © 2016 Manoj Chakkaravarthy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
